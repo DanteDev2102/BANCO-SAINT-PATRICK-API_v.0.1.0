8b540eb4646a96eca72f7d067cb07c1810438e37
Jose Acosta <jdap.dmc@gmail.com>
2022-01-31 08:22:19 -0400

    feat: express-session and passport middlewares

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const passport = require('passport');
+const session = require('express-session');
 
 const { connectDB } = require('./network/database');
 const routes = require('./network/routes');
-const { portServer, hostServer } = require('./network/config');
+const {
+	portServer,
+	hostServer,
+	sessionSecret
+} = require('./network/config');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(
+	session({
+		secret: sessionSecret,
+		name: 'express-session',
+		resave: false,
+		saveUninitialized: true
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 routes(app);
 
--- network/config.js
+++ network/config.js
@@ -6,7 +6,8 @@ const config = {
 	portServer: process.env.PORT_SERVER ?? 3001,
 	hostServer: process.env.HOST_SERVER ?? 'http://localhost',
 	dbName: process.env.DATABASE ?? 'banco_saint_patrick',
-	dbPort: process.env.DATABASE_PORT ?? 3306
+	dbPort: process.env.DATABASE_PORT ?? 3306,
+	sessionSecret: process.env.SESSION_SECRET ?? 'this is a secret'
 };
 
 module.exports = config;
